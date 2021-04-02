435ceec950adf20ec3d97989a66876e85ed41044
Azzedine <54241944+Arsero@users.noreply.github.com>
2021-04-02 16:28:34 +0200

    Add service component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ServiceComponent } from './components/service/service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
